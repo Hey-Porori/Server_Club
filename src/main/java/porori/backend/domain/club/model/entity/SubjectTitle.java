12c569cae5eb0baeaefbdf5514b0eb12c3c59899
Kim Ahyun <kimahyunn132@gmail.com>
2023-08-13 09:01:53 +0900

    feat: String을 enum으로 반환하는 dto 기능 추가

--- src/main/java/porori/backend/domain/club/model/entity/SubjectTitle.java
+++ src/main/java/porori/backend/domain/club/model/entity/SubjectTitle.java
@@ -3,19 +3,30 @@ package porori.backend.domain.club.model.entity;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.util.Arrays;
+import java.util.Objects;
+
 @Getter
 @AllArgsConstructor
 public enum SubjectTitle {
-    TRAVEL(0),
-    COOK(1),
-    LANGUAGE(2),
-    SPORT(3),
-    ART(4),
-    SCIENCE(5),
-    MUSIC(6),
-    ENVIRONMENT(7),
-    BOOK(8);
+    TRAVEL("여행"),
+    COOK("요리"),
+    LANGUAGE("언어"),
+    SPORT("운동"),
+    ART("미술"),
+    SCIENCE("과학"),
+    MUSIC("음악"),
+    ENVIRONMENT("환경"),
+    BOOK("독서"),
+    EMPTY("");
 
-    private final int index;
+    private final String title;
+
+    public static SubjectTitle valueOfTitle(String title) {
+        return Arrays.stream(SubjectTitle.values())
+                .filter(SubjectTitle -> Objects.equals(title, SubjectTitle.getTitle()))
+                .findFirst()
+                .orElse(EMPTY);
+    }
 
 }
