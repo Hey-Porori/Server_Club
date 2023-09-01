3eb02567e9959fd7349674833f395ee211387dd5
Kim Ahyun <kimahyunn132@gmail.com>
2023-09-01 13:55:51 +0900

    feat: Success 상태 추가
    동호회 신청 완료, 동호회 멤버 등록 완료

--- src/main/java/porori/backend/global/common/status/SuccessStatus.java
+++ src/main/java/porori/backend/global/common/status/SuccessStatus.java
@@ -8,7 +8,9 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum SuccessStatus {
 
-    CREATE_CLUB(201, "동호회 등록이 완료되었습니다.");
+    CREATE_CLUB(201, "동호회 등록이 완료되었습니다."),
+    CREATE_APPLICATION(201, "동호회 신청이 완료되었습니다."),
+    CREATE_MEMBER(201, "동호회 멤버 등록이 완료되었습니다.");
 
     private final int statusCode;
     private final String message;
