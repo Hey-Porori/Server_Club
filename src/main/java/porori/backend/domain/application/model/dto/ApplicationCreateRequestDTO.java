f5afdc6a09d4c2e91c81798a52c1c5259df1e22b
Kim Ahyun <kimahyunn132@gmail.com>
2023-09-01 13:54:50 +0900

    feat: Application 생성 DTO 추가

---
+++ src/main/java/porori/backend/domain/application/model/dto/ApplicationCreateRequestDTO.java
@@ -0,0 +1,11 @@
+package porori.backend.domain.application.model.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class ApplicationCreateRequestDTO {
+
+    private Long clubId;
+}
