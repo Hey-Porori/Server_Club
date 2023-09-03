df449bb15f976a932c45bc8bafe3ba050c1ef817
Kim Ahyun <kimahyunn132@gmail.com>
2023-09-03 14:52:07 +0900

    style: DTO에 설명 추가
    swagger에 설명 추가를 위해 Schema description, example 추가

--- src/main/java/porori/backend/domain/application/model/dto/ApplicationCreateResponseDTO.java
+++ src/main/java/porori/backend/domain/application/model/dto/ApplicationCreateResponseDTO.java
@@ -1,17 +1,24 @@
 package porori.backend.domain.application.model.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import porori.backend.domain.application.model.entity.Application;
 import porori.backend.domain.application.model.entity.ApplicationStatus;
 
+@Schema(description = "동호회 가입 신청 Response : 동호회 가입 신청 후 얻을 수 있는 정보")
 @Builder(access = AccessLevel.PRIVATE)
 @Getter
 public class ApplicationCreateResponseDTO {
 
+    @Schema(description = "유저 ID", example = "1")
     private Long userId;
+
+    @Schema(description = "동호회 ID", example = "1")
     private Long clubId;
+
+    @Schema(description = "지원 상태", example = "APPLIED")
     private ApplicationStatus applicationStatus;
 
     public static ApplicationCreateResponseDTO from(Application application) {
--- src/main/java/porori/backend/domain/club/model/dto/ClubCreateResponseDTO.java
+++ src/main/java/porori/backend/domain/club/model/dto/ClubCreateResponseDTO.java
@@ -1,13 +1,17 @@
 package porori.backend.domain.club.model.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import porori.backend.domain.club.model.entity.Club;
 
+@Schema(description = "동호회 생성 Response : 동호회 생성 후 얻을 수 있는 정보")
 @Builder(access = AccessLevel.PRIVATE)
 @Getter
 public class ClubCreateResponseDTO {
+
+    @Schema(description = "동호회 ID", example = "1")
     private Long clubId;
 
     public static ClubCreateResponseDTO from(Club club) {
