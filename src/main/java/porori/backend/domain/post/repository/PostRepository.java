7a2fc6579d1353e684a117889dc2fdd2b26f0ed0
Kim Ahyun <kimahyunn132@gmail.com>
2023-10-17 19:01:02 +0900

    feat: Club과 상태로 Post를 반환하는 레포지토리 단 메소드

--- src/main/java/porori/backend/domain/post/repository/PostRepository.java
+++ src/main/java/porori/backend/domain/post/repository/PostRepository.java
@@ -2,8 +2,14 @@ package porori.backend.domain.post.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import porori.backend.domain.club.model.entity.Club;
 import porori.backend.domain.post.model.entity.Post;
+import porori.backend.global.common.status.BaseStatus;
+
+import java.util.List;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findByClubAndStatus(Club club, BaseStatus status);
 }
