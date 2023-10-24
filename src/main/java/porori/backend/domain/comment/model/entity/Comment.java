ea180ed8766717babd85f6eb558bca3f5c10d97b
Kim Ahyun <kimahyunn132@gmail.com>
2023-10-24 22:20:46 +0900

    feat: Comment Entity 구현
    일대다 관계가 있는 Post Entity 수정도 수행

---
+++ src/main/java/porori/backend/domain/comment/model/entity/Comment.java
@@ -0,0 +1,38 @@
+package porori.backend.domain.comment.model.entity;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import porori.backend.domain.post.model.entity.Post;
+import porori.backend.global.common.entity.BaseEntity;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentId;
+
+    @Column(nullable = false)
+    private Long userId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Builder
+    protected Comment(Long commentId, Long userId, Post post, String content) {
+        this.commentId = commentId;
+        this.userId = userId;
+        this.post = post;
+        this.content = content;
+    }
+}
