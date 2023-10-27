9ed00b045d005d122ee8068f0a888b3be59268a1
Kim Ahyun <kimahyunn132@gmail.com>
2023-10-27 23:02:55 +0900

    feat: commentId와 userId로 Comment의 존재를 확인하는 메소드 추가

--- src/main/java/porori/backend/domain/comment/repository/CommentRepository.java
+++ src/main/java/porori/backend/domain/comment/repository/CommentRepository.java
@@ -12,4 +12,6 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     List<Comment> findByPostAndStatus(Post post, BaseStatus status);
+
+    boolean existsByCommentIdAndUserId(Long commentId, Long userId);
 }
