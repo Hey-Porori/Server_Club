ab62b6684895904c4e968d693fecfe5b194df1ff
Kim Ahyun <kimahyunn132@gmail.com>
2023-10-30 21:12:59 +0900

    refactor: Member 관련 로직에서 검증 로직 추출

--- src/main/java/porori/backend/domain/member/service/MemberService.java
+++ src/main/java/porori/backend/domain/member/service/MemberService.java
@@ -14,6 +14,7 @@ import porori.backend.domain.member.model.entity.Role;
 import porori.backend.domain.member.repository.MemberRepository;
 import porori.backend.domain.user.service.UserService;
 import porori.backend.global.common.status.BaseStatus;
+import porori.backend.global.utils.Validator;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -30,6 +31,8 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
     private final ClubRepository clubRepository;
+
+    private final Validator validator;
 
     public void addMember(Club club, Long userId, Role role) {
         Club findClub = clubRepository.findById(club.getClubId()).orElseThrow(() -> new ClubException(INVALID_CLUB));
@@ -53,21 +56,17 @@ public class MemberService {
 
     public List<MemberResponseDTO> getMemberList(String token, Long clubId) {
         Long managerId = userService.getUserId(token);
-        Club club = verifyClubManager(clubId, managerId);
+        validator.verifyClubManager(clubId, managerId);
+        Club club = clubRepository.findById(clubId).orElseThrow(() -> new ClubException(INVALID_CLUB));
 
         List<Member> members = memberRepository.findByClubAndStatus(club, ACTIVE);
         return userService.getMemberResponseDTO(members.stream().map(Member::getUserId).collect(Collectors.toList()));
     }
 
-    private Club verifyClubManager(Long clubId, Long userId) {
-        if (!clubRepository.existsByClubIdAndUserId(clubId, userId))
-            throw new ClubException(NOT_CLUB_MANAGER);
-        return clubRepository.findById(clubId).orElseThrow(() -> new ClubException(INVALID_CLUB));
-    }
-
     public MemberStatusResponseDTO kickOutMember(String token, Long clubId, Long userId) {
         Long managerId = userService.getUserId(token);
-        Club club = verifyClubManager(clubId, managerId);
+        validator.verifyClubManager(clubId, managerId);
+        Club club = clubRepository.findById(clubId).orElseThrow(() -> new ClubException(INVALID_CLUB));
 
         Member member = memberRepository.findByClubAndUserId(club, userId).orElseThrow(() -> new MemberException(NOT_EXIST_MEMBER));
 
