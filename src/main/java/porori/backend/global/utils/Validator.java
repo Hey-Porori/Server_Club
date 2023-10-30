99e7eaf568f395c422ec0cc4241123f73f534805
Kim Ahyun <kimahyunn132@gmail.com>
2023-10-30 21:03:12 +0900

    feat: Validator 클래스 추가
    verify를 수행하는 메소드들을 가지는 클래스

---
+++ src/main/java/porori/backend/global/utils/Validator.java
@@ -0,0 +1,9 @@
+package porori.backend.global.utils;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class Validator {
+}
